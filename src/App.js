e5ba2818471b18c6cdf7319d6e67c1dbb5c21ddc
Alyssa Wendt <alyssa95wendt@gmail.com>
2023-05-06 10:28:10 -0400

    adding App.js code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import Canvas from './components/Canvas';
+import Tree from './components/Tree';
 import './App.css';
 
 function App() {
   return (
-    <Tree/>
+   <div className="App">
+    <h1>Fractal Tree</h1>
+    <Canvas width={800} height={600} draw={Tree} />
+   </div>
   );
 }
 
